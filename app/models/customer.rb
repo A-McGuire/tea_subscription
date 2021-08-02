f52a59c0c0c737b0b6e0a74299bfa9a435111b72
Aidan McGuire <aidanmcguire211@gmail.com>
2021-08-02 15:15:42 -0600

    Add customer validations and relationships

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,12 @@
 class Customer < ApplicationRecord
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
+  validates :street, presence: true
+  validates :state, presence: true
+  validates :zip, presence: true
+  validates :city, presence: true
+
+  has_many :customer_subscriptions, dependent: :destroy
+  has_many :subscriptions, through: :customer_subscriptions
 end
