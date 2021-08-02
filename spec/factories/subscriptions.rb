118385231d8957e99f7835fd2e533edad926c217
Aidan McGuire <aidanmcguire211@gmail.com>
2021-08-02 13:39:28 -0600

    Update subscriptions factory

--- spec/factories/subscriptions.rb
+++ spec/factories/subscriptions.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :subscription do
-    title { Faker::SuperHero.name }
+    title { "#{Faker::Artisrt.name} collection" }
     frequency { 1 }
     price { "9.99" }
   end
