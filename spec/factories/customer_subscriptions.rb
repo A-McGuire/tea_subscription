f1a20f5bce7c5c8e642b64a7eead0ec4af00abb2
Aidan McGuire <aidanmcguire211@gmail.com>
2021-08-02 16:14:10 -0600

    Fix copy paste error

--- spec/factories/customer_subscriptions.rb
+++ spec/factories/customer_subscriptions.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
-  factory :customer_subscriptions do
+  factory :customer_subscription do
     subscription_id { 1 }
-    tea_id { 1 }
+    customer_id { 1 }
     status { [1,2,3].sample }
   end
 end
