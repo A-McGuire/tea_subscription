f18b400e5d3ff72db906ce6d68ab51fab05c6749
Aidan McGuire <aidanmcguire211@gmail.com>
2021-08-02 15:16:29 -0600

    Add tea validations and relationships

--- app/models/tea.rb
+++ app/models/tea.rb
@@ -1,2 +1,9 @@
 class Tea < ApplicationRecord
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :brew_time, presence: true
+
+  has_many :drink_subscriptions, dependent: :destroy
+  has_many :subscriptions, through: :drink_subscriptions
 end
+
