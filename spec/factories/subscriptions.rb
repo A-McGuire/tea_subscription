85909e4816454662826c5dfdbfbb9757e06c2ec6
Aidan McGuire <aidanmcguire211@gmail.com>
2021-08-02 14:04:07 -0600

    Change subscriptions factory frequency attr to array

--- spec/factories/subscriptions.rb
+++ spec/factories/subscriptions.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :subscription do
     title { Faker::Books::CultureSeries.culture_ship }
-    frequency { 1 }
+    frequency { [1,2,3,4].sample }
     price { "9.99" }
   end
 end
