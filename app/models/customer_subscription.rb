f1a1fa4522f7be5a57245b52bc80e0f8017e86f0
Aidan McGuire <aidanmcguire211@gmail.com>
2021-08-02 15:16:01 -0600

    Add customer_subscription validations and relationships

--- app/models/customer_subscription.rb
+++ app/models/customer_subscription.rb
@@ -1,2 +1,7 @@
 class CustomerSubscription < ApplicationRecord
+  validates :customer_id, presence: true, numericality: true
+  validates :subscription_id, presence: true, numericality: true
+
+  belongs_to :customer
+  belongs_to :subscription
 end
