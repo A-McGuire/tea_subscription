41115e76cde7ef16a7211203a533a56c028983e0
Aidan McGuire <aidanmcguire211@gmail.com>
2021-08-02 15:15:11 -0600

    Add specs for tea validations and relationships

--- spec/models/tea_spec.rb
+++ spec/models/tea_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Tea, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'validations' do
+    it {should validate_presence_of(:title)}
+    it {should validate_presence_of(:description)}
+    it {should validate_presence_of(:brew_time)}
+  end
+
+  context 'relationships' do
+    it {should have_many(:drink_subscriptions)}
+    it {should have_many(:subscriptions).through(:drink_subscriptions)}
+  end
 end
