bd31ed81f953de5f8db6a753f1e75232e434d402
Aidan McGuire <aidanmcguire211@gmail.com>
2021-08-02 15:16:20 -0600

    Add subscription validations and relationships

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,2 +1,11 @@
 class Subscription < ApplicationRecord
+  validates :title, presence: true
+  validates :frequency, presence: true
+  validates :price, presence: true
+
+  has_many :drink_subscriptions, dependent: :destroy
+  has_many :teas, through: :drink_subscriptions
+
+  has_many :customer_subscriptions, dependent: :destroy
+  has_many :customers, through: :customer_subscriptions
 end
