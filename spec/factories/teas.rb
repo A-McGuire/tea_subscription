543ccbbaca21dd15d29132c0de15adf4bbbe1d41
Aidan McGuire <aidanmcguire211@gmail.com>
2021-08-02 16:14:35 -0600

    Fix typo in tea fatory

--- spec/factories/teas.rb
+++ spec/factories/teas.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :tea do
-    title { Faker::SuperHero.name }
+    title { Faker::Superhero.name }
     description { Faker::GreekPhilosophers.quote }
     brew_time { [1,2,3,4,5].sample }
   end
