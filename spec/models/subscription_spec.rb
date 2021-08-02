06b87039b89e095602bc4418b70f231f1a1c08f8
Aidan McGuire <aidanmcguire211@gmail.com>
2021-08-02 15:14:53 -0600

    Add specs for subscription validations and relationships

--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Subscription, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'validations' do
+    it {should validate_presence_of(:title)}
+    it {should validate_presence_of(:frequency)}
+    it {should validate_presence_of(:price)}
+  end
+
+  context 'relationships' do
+    it {should have_many(:drink_subscriptions)}
+    it {should have_many(:teas).through(:drink_subscriptions)}
+    
+    it {should have_many(:customer_subscriptions)}
+    it {should have_many(:customers).through(:customer_subscriptions)}
+  end
 end
