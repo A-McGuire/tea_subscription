08ca37d2f80221ff393118bd211501e6f1c6c3f6
Aidan McGuire <aidanmcguire211@gmail.com>
2021-08-02 15:14:01 -0600

    Add specs for customer validations and relationships

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'validations' do
+    it {should validate_presence_of(:first_name)}
+    it {should validate_presence_of(:last_name)}
+    it {should validate_presence_of(:email)}
+    it {should validate_presence_of(:street)}
+    it {should validate_presence_of(:city)}
+    it {should validate_presence_of(:state)}
+    it {should validate_presence_of(:zip)}
+  end
+
+  context 'relationships' do
+    it {should have_many(:customer_subscriptions)}
+    it {should have_many(:subscriptions).through(:customer_subscriptions)}
+  end
 end
