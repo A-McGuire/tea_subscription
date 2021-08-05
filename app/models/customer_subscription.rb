da3b96c11b6069690403f83e183392bd42a878b1
Aidan McGuire <aidanmcguire211@gmail.com>
2021-08-04 19:02:20 -0600

    Remove status enum

--- app/models/customer_subscription.rb
+++ app/models/customer_subscription.rb
@@ -2,8 +2,6 @@ class CustomerSubscription < ApplicationRecord
   validates :customer_id, presence: true, numericality: true
   validates :subscription_id, presence: true, numericality: true
 
-  enum status: [:inactive, :active, :cancelled], _default: :inactive
-
   belongs_to :customer
   belongs_to :subscription
 end
