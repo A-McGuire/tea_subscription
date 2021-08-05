caf589ff8035b7f99ea1120bc3d47bf98f0d8585
Aidan McGuire <aidanmcguire211@gmail.com>
2021-08-04 19:05:35 -0600

    Add update action

--- app/controllers/api/v1/subscriptions_controller.rb
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -1,7 +1,15 @@
 class Api::V1::SubscriptionsController < ApplicationController
   def index
+    return render status: :unprocessable_entity unless params[:id]
     subscriptions = SubscriptionsFacade.format_all_customer_subscriptions(params[:id])
 
     render json: SubscriptionsSerializer.new(subscriptions).serializable_hash
+  end
+
+  def update
+    return render status: :unprocessable_entity unless params[:operation] && params[:subscription_id]
+    updated_subscription = SubscriptionsFacade.update_subscription({customer_id: params[:id], operation: params[:operation], subscription_id: params[:subscription_id]})
+
+    updated_subscription ? (render status: :no_content) : (render status: :bad_request)
   end
 end
